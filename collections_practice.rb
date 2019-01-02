53257d795bbd6e54adc736f80f4282afea45e5ec
Maximo Bautista <max.bautista97@gmail.com>
2019-01-02 14:35:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,7 +56,7 @@ def find_cool(array_of_hashes)
 end
   
 def organize_schools(hash_of_schools)
-  hash_of_schools.group_by {|key, value| value[:location] }
+  hash_of_schools.select {|key, value| value[:location] }
     
 end
   
